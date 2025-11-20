3a4f95065263b98e1c36e32d591c511dfaad30ee
wanda azhar <wandaazhar@gmail.com>
2025-11-20 05:14:49 -0700

    wanda: adding whychooseus section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedProductsPreview from "./components/featuredProducts/FeaturedProductsPreview";
 import HeroSection from "./components/heroSection/HeroSection";
+import WhyChooseUsSection from "./components/whyChooseUs/WhyChooseUsSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <FeaturedProductsPreview />
+      <WhyChooseUsSection />
     </>
   );
 }
