879de385b5d6b0a18377d7a327d69a044b7a8279
wanda azhar <wandaazhar@gmail.com>
2025-11-20 20:42:31 -0700

    wanda: Creating a contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+import BusinessHoursSection from "../components/contact/businessHours/BusinessHoursSection";
+import CtaContactSection from "../components/contact/ctaContact/ctaContactSection";
+
+const Contact = () => {
+  return (
+    <>
+      <BusinessHoursSection />
+      <CtaContactSection />
+    </>
+  );
+}
+
+export default Contact;
